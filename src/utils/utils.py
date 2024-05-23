d81360c208e275beda0cfa851f97009f933a1c4c
Francesco Peragine <5671795+francescoperagine@users.noreply.github.com>
2024-05-23 18:45:27 +0200

    add utils and visuals

---
+++ src/utils/utils.py
@@ -0,0 +1,115 @@
+from sklearn.preprocessing import StandardScaler
+from technical_analysis import candles as ta_candles
+import pandas as pd
+from sklearn.decomposition import PCA
+from sklearn.feature_selection import mutual_info_classif
+import numpy as np
+
+def get_features(df: pd.DataFrame) -> list:
+    """Return a list of features in the DataFrame."""	
+    return [x for x in df.columns]
+
+def get_null_values(df: pd.DataFrame) -> pd.DataFrame:
+    """Return the rows with null values."""	
+    return df[df['null'] == True]
+
+def get_dataframe_null_summary(df: pd.DataFrame, name: str) -> dict:
+    """Return a summary of the DataFrame including the total entries, null entries, null percentage, and zero count."""	
+    null_df = get_null_values(df)
+    total_entries = len(df)
+    null_entries = len(null_df)
+    null_percentage = null_entries / total_entries if total_entries > 0 else 0
+
+    # Calculate zero count
+    zero_count = (df == 0).sum()
+    
+    return {
+        'Exchange': name,
+        'Total Entries': total_entries,
+        'Null Entries': null_entries,
+        'Null Percentage (%)': round(null_percentage, 5),
+        **zero_count.to_dict()  # Expand the zero_count Series into the dictionary
+    }
+
+def reduce_cumulative_variance(array: np.array, tolerance: float =1e-3) -> np.array:
+    array = np.array(array)
+    condition = array >= 1.0 - tolerance
+    index = np.argmax(condition) if np.any(condition) else len(array)
+    return array[:index + 1]
+
+def standard_scale(df: pd.DataFrame) -> pd.DataFrame:
+    """Scale the DataFrame using StandardScaler."""	
+    scaler = StandardScaler()
+    return pd.DataFrame(scaler.fit_transform(df), columns=df.columns, index=df.index)
+
+def perform_pca(df: pd.DataFrame, variance_threshold: float) -> tuple[PCA, np.array, np.array]:
+    """Perform PCA and return the PCA object."""
+
+    # Perform PCA
+    pca = PCA()
+    pca = pca.fit(df)
+    explained_variance = pca.explained_variance_ratio_
+    cumulative_variance = explained_variance.cumsum()
+
+    # Get the number of components that explain the variance threshold
+    num_components = np.where(cumulative_variance >= variance_threshold)[0][0] + 1
+    pca_reduced = PCA(n_components=num_components).fit(df)
+    
+    return pca_reduced, explained_variance[:num_components], cumulative_variance[:num_components]
+
+def compute_loadings(pca: PCA, df: pd.DataFrame) -> pd.DataFrame:
+    """Compute the PCA loadings and return them as a DataFrame."""
+    return pd.DataFrame(pca.components_.T, columns=[f'PC_{i+1}' for i in range(pca.n_components_)], index=df.columns)
+
+def add_indicators(df: pd.DataFrame) -> pd.DataFrame:
+    """Add technical indicators to the DataFrame."""	
+    new_df = df.copy()
+    new_df['doji'] = ta_candles.is_doji(new_df['open'], new_df['high'], new_df['low'], new_df['close']).astype(int)
+    new_df['bullish_engulfing'] = ta_candles.bullish_engulfing(new_df['open'], new_df['high'], new_df['low'], new_df['close']).astype(int)
+    return new_df
+
+def add_orderbook_value_feature(df, orders=20):
+    """Add price * size product features to orderbook DataFrame."""
+    new_df = df.copy()
+    for i in range(orders):
+        new_df.insert(new_df.columns.get_loc(f'bid_{i}_size') + 1, f'bid_{i}_value', 0) 
+        new_df[f'bid_{i}_value'] = new_df[f'bid_{i}_price'] * new_df[f'bid_{i}_size']
+        new_df[f'ask_{i}_value'] = new_df[f'ask_{i}_price'] * new_df[f'ask_{i}_size']
+    return new_df
+
+def get_information_gain(X: pd.DataFrame, y: pd.DataFrame) -> pd.DataFrame:
+    """Compute the information gain for each feature and return it as a DataFrame."""	
+    ig_scores = mutual_info_classif(X, y)
+    return pd.DataFrame(ig_scores, index=X.columns, columns=[f'Information_Gain'])
+
+def find_elbow_point(combined_scores: pd.DataFrame) -> int:
+    """Find the elbow point in the scores DataFrame and return the index."""	
+
+    # Calculate the angle between points (naive elbow detection)
+    sorted_scores = combined_scores.iloc[:, 0].sort_values(ascending=False).values
+    if len(sorted_scores) > 2:  # Ensure there are enough points to calculate second derivative
+        angles = np.arctan(np.diff(sorted_scores, n=2))
+        if len(angles) > 0:
+            elbow = np.argmin(angles) + 1  # +1 as diff reduces the original index by 1
+            return elbow
+    return 0
+
+def compare_features_scores(pca_loadings: pd.DataFrame, info_scores: pd.DataFrame):
+    # Normalize PCA loadings and MI scores
+    
+    # Euclidean norm of PCA loadings for quantifying the overall importance of each feature across all components
+    loadings_norm = np.linalg.norm(pca_loadings, axis=1)
+
+    # Min-max normalization
+    loadings_norm = pd.DataFrame(loadings_norm, index=pca_loadings.index, columns=['Loadings_Norm'])
+    loadings_norm_scaled = (loadings_norm - loadings_norm.min()) / (loadings_norm.max() - loadings_norm.min())
+
+    info_gain_scaled = (info_scores - info_scores.min()) / (info_scores.max() - info_scores.min())
+    
+    combined_scores = pd.DataFrame({
+        'Loadings_Norm': loadings_norm_scaled.squeeze(),
+        'Information_Gain': info_gain_scaled.squeeze()
+    })
+
+    combined_scores['Combined_Scores'] = pd.DataFrame((combined_scores['Loadings_Norm'] + combined_scores['Information_Gain']) / 2, columns=['Combined_Scores'])
+    return combined_scores
---
+++ src/visualization/visualize.py
@@ -0,0 +1,132 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+import pandas as pd
+
+from src.utils.constants import *
+
+def plot_correlation_matrix(exchange: str, data_type: str, correlation_matrix: pd.DataFrame):
+    plt.figure(figsize=(12, 10))
+    sns.heatmap(correlation_matrix, annot=False, cmap='coolwarm')
+    plt.title(f'{exchange}-{data_type} correlation matrix of Crypto Market Features')
+    plt.savefig(os.path.join(FIGURE_PATH, f'{exchange}_{data_type}_correlation_matrix.png'))
+    plt.show()
+
+def plot_pca_variance(data_type: str, exchange: str, explained_variance: np.ndarray, cumulative_variance: np.ndarray):
+    components = np.arange(1, len(explained_variance) + 1)
+
+    if data_type == ORDERBOOKS:
+        plt.figure(figsize=(20, 6))
+    else:
+        plt.figure(figsize=(6,4))
+
+    bars = plt.bar(components, explained_variance, alpha=0.6, color='g', label='Explained variance')
+    plt.plot(components, cumulative_variance, marker='o', linestyle='-', color='r', label='Cumulative variance')
+
+    # Individual explained variance - skip the first bar
+    for idx, bar in enumerate(bars):
+        yval = bar.get_height()
+        if idx != 0:  # Skip the first bar
+            plt.text(bar.get_x() + bar.get_width()/2, yval, f'{yval:.3f}', fontsize=8, va='bottom', ha='center')
+
+    # Cumulative explained variance on the line plot
+    for i, txt in enumerate(cumulative_variance):
+        plt.text(components[i], cumulative_variance[i], f'{txt:.3f}', fontsize=8, ha='right', va='bottom')
+
+    plt.title(f'Variance Explained by PCA Components for {exchange} {data_type}')
+    plt.xlabel('Principal Component')
+    plt.ylabel('Variance Explained')
+    plt.xticks(components)
+    plt.legend(loc='center right')
+    plt.tight_layout()
+    plt.savefig(os.path.join(FIGURE_PATH, f'{exchange}_{data_type}_pca_variance.png'))
+    plt.show()
+
+# Plot the PCA loadings
+def plot_loadings(data_type: str, exchange: str, loadings: pd.DataFrame):
+    if data_type == ORDERBOOKS:
+        plt.figure(figsize=(15, 15))
+    else:
+        plt.figure(figsize=(13, 5))
+    loadings.plot(kind='bar', width=3)
+    plt.title(f'PCA Loadings for {exchange}')
+    plt.savefig(os.path.join(FIGURE_PATH, f'{exchange}_{data_type}_pca_loadings.png'))
+    plt.show()
+
+# Plot the PCA loadings as a heatmap   
+def plot_loadings_heatmap(data_type: str, exchange: str, loadings: pd.DataFrame, component_index: int = 0):
+    font_size = 9
+    # loadings = loadings.sort_values(by=loadings.columns[0], ascending=False)
+    sorted_loadings = loadings.sort_values(by=loadings.columns[component_index], ascending=False)
+
+    # Adjust the figure size dynamically based on the number of features
+    height = max(6, min(loadings.shape[0] * 0.5, 20))  
+    width = max(10, min(loadings.shape[1] * 1.2, 20))
+    annot = False if data_type == ORDERBOOKS else True
+    
+    if data_type == ORDERBOOKS:
+        plt.figure(figsize=(width, height))
+        ax = sns.heatmap(loadings, cmap='coolwarm', annot=annot)
+    else:
+        plt.figure(figsize=(width, height))
+        ax = sns.heatmap(sorted_loadings, cmap='coolwarm', annot=annot, annot_kws={'size': font_size - 2})
+
+    plt.title(f'PCA Loadings for {exchange}', fontsize=font_size + 2)
+    plt.xlabel('Principal Components', fontsize=font_size)
+    plt.ylabel('Features', fontsize=font_size)
+    ax.tick_params(axis='both', which='major', labelsize=font_size)
+    plt.tight_layout()
+    plt.savefig(os.path.join(FIGURE_PATH, f'{exchange}_{data_type}_pca_loadings_heatmap.png'))
+    plt.show()
+
+# Plot the combined scores as a histogram and density plot
+def plot_histogram_density(data_type: str, exchange: str, df: pd.DataFrame, labels: list, colors: list):
+    plt.figure(figsize=(12, 8))
+    for label, color in zip(labels, colors):
+        # Plotting the histogram
+        sns.histplot(df[label], color=color, label=f'{label} Histogram', alpha=0.5, edgecolor='k', bins=30)
+        # Plotting the density
+        sns.kdeplot(df[label], color=color, label=f'{label} Density', lw=3)
+    
+    plt.title('Histogram and Density Plots of Feature Scores')
+    plt.xlabel('Score')
+    plt.ylabel('Frequency')
+    plt.legend(title='Legend')
+    plt.grid(True)
+    plt.savefig(os.path.join(FIGURE_PATH, f'{exchange}_{data_type}_combined_scores_histogram_density.png'))
+    plt.show()
+
+# Plot the elbow plot to find the optimal number of features
+def plot_elbow_curve(data_type: str, exchange: str, combined_scores, elbow_index):
+    """Plot the sorted scores and highlight the elbow point."""
+    
+    sorted_scores = combined_scores.iloc[:, 0].sort_values(ascending=False).values
+    
+    plt.figure(figsize=(10, 6))
+    plt.plot(sorted_scores, 'o-', label='Combined Scores')
+    if elbow_index > 0 and elbow_index < len(sorted_scores):  # Check if elbow_index is valid
+        plt.plot(elbow_index, sorted_scores[elbow_index], 'ro', label='Elbow Point')
+        plt.annotate('Elbow', xy=(elbow_index, sorted_scores[elbow_index]),
+                     xytext=(elbow_index + 10, sorted_scores[elbow_index] - 0.1),
+                     arrowprops=dict(facecolor='black', shrink=0.05))
+    plt.title('Elbow Method to Determine Optimal Number of Features')
+    plt.xlabel('Feature Index')
+    plt.ylabel('Score')
+    plt.legend()
+    plt.grid(True)
+    plt.savefig(os.path.join(FIGURE_PATH, f'{exchange}_{data_type}_combined_scores_elbow_plot.png'))
+    plt.show()
+
+def plot_tree_learning_curves(exchange: str, data_type: str, depths: int, train_scores: list, test_scores: list):
+    """Plot the learning curves for the training and test sets."""
+
+    plt.figure(figsize=(10, 6))
+    plt.plot(depths, train_scores, label='Train Score', marker='o')
+    plt.plot(depths, test_scores, label='Test Score', marker='o')
+    plt.xlabel('Tree Depth')
+    plt.ylabel('Accuracy')
+    plt.title(f'Learning Curves for {exchange} {data_type}')
+    plt.legend()
+    plt.grid(True)
+    plt.savefig(os.path.join(FIGURE_PATH, f'{exchange}_{data_type}_learning_curves.png'))
+    plt.show()
